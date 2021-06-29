fe6e2f98d4a8c408ab75b79a88bff61aa9c12b26
AlgorithmCodes <krishna.reddy501501@gmail.com>
2021-06-29 06:54:42 +0530

    third commit

---
+++ src/programs1_10/Pr_3.java
@@ -0,0 +1,12 @@
+package programs1_10;
+
+public class Pr_3 {
+	
+	public static void main(String[] args) {
+		float input1=9.0f;
+		float input2=4.6f;
+		float multiplication=input1*input2;
+		System.out.println("floating point numbers multiplication is:"+multiplication);
+	}
+
+}
